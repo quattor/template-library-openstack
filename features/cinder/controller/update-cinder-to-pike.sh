48782ce0ab49a8688a1847c425bd3d60689d7442
apdibbo <apdibbo@clearskies.it>
2018-06-11 15:07:28 +0100

    Changes to add endpoints for cinder volume v3

--- features/cinder/controller/update-cinder-to-pike.sh
+++ features/cinder/controller/update-cinder-to-pike.sh
@@ -3,9 +3,19 @@ echo "load variable"
 
 %s
 
+export CINDERV3_URL="https://%s:8776/v3/%%\(project_id\)s"
+
 echo "[START] Databases upgrade"
 echo "  Block service"
 $DEBUG_DATABASES systemctl stop openstack-cinder*
 $DEBUG_DATABASES su -s /bin/sh -c "cinder-manage db sync" cinder
 $DEBUG_DATABASES systemctl start openstack-cinder-api openstack-cinder-scheduler openstack-cinder-volume
 echo "[DONE] Database configuration"
+
+echo "[START] configure volumev3"
+$DEBUG_SERVICES openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
+for endpoint_type in $ENDPOINT_TYPES $ADMIN_ENDPOINT_TYPE
+do
+  $DEBUG_ENDPOINTS openstack endpoint create --region $REGION 'volumev3' $endpoint_type $REGION $CINDERV3_URL
+done
+echo "[DONE] configure volumev3"
