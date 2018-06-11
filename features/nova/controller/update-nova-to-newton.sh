8e4e8c4ac8b2703b12b24bda731e65459d44a7dc
apdibbo <apdibbo@clearskies.it>
2018-06-11 13:31:38 +0100

    Add online database migrations for Nova

--- features/nova/controller/update-nova-to-newton.sh
+++ features/nova/controller/update-nova-to-newton.sh
@@ -6,4 +6,5 @@ echo "[START] Databases upgrades"
 echo "  Compute service"
 $DEBUG_DATABASES su -s /bin/sh -c "nova-manage api_db sync" nova
 $DEBUG_DATABASES su -s /bin/sh -c "nova-manage db sync" nova
+$DEBUG_DATABASES su -s /bin/sh -c "nova-manage db online_data_migrations" nova
 echo "[DONE] Database configuration"
